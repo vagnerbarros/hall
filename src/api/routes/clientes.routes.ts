4b9f5b57a192eb6c4ad424e733da1c7579a65a45
Vagner Barros <vagnerbarrospereira@gmail.com>
2023-04-11 21:35:13 -0300

    Rota de cadastro de clientes

--- src/api/routes/clientes.routes.ts
+++ src/api/routes/clientes.routes.ts
@@ -1,14 +1,25 @@
 import { Request, Response, Router } from "express";
 import { clientes } from "../../mocks/clientes.mock";
+import { Cliente } from "../../domain/model/clientes";
 
 export default class ApiClientes {
-  constructor() {}
+  private clientes: Cliente[];
+  constructor() {
+    this.clientes = [];
+  }
 
   routes(): Router {
     const router: Router = Router();
 
     router.get("", async (req: Request, res: Response) => {
-      res.send({ clientes });
+      res.send({ clientes: this.clientes });
+    });
+
+    router.post("", (req: Request, res: Response) => {
+      const novoCliente = req.body as Cliente;
+      novoCliente.id = this.clientes.length + 1;
+      this.clientes.push(novoCliente);
+      res.send({ ...novoCliente });
     });
 
     return router;
